3b1ce086fa9186bc0372c2cb004f279aed1d3e94
pajamacoder <codespectator@gmail.com>
2022-02-20 15:00:09 +0000

    M: add desker model v2

--- modules/dataset/build.py
+++ modules/dataset/build.py
@@ -3,10 +3,10 @@ from .ocrDataset import OCRDataset
 
 ocrFactory = {'OCRDataset':OCRDataset}
 
-def build_dataloader(type, batch_size, train, valid, augment_fn):
+def build_dataloader(type, train_batch, valid_batch, train, valid, augment_fn):
     train_dataset = ocrFactory[type](**train, transformer=augment_fn)
     valid_dataset = ocrFactory[type](**valid, transformer=augment_fn)
     sampler = RandomSampler(train_dataset)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=(sampler is None), sampler=sampler)
-    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=train_batch, shuffle=(sampler is None), sampler=sampler)
+    valid_loader = DataLoader(valid_dataset, batch_size=valid_batch, shuffle=False)
     return train_loader, valid_loader
--- modules/model/build.py
+++ modules/model/build.py
@@ -1,6 +1,8 @@
 from .rotation_model import DeskewNet
+from .rotation_model_v2 import DeskewNetV2
 models={
-    "DeskewNet":DeskewNet
+    "DeskewNet":DeskewNet,
+    "DeskewNetV2":DeskewNetV2,
 }
 def build_model(type, args):
     return models[type](**args)
